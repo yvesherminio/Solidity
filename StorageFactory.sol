505fbb86bc6839c84097028ddeca4fbf18d808ef
yvesherminio <71796591+yvesherminio@users.noreply.github.com>
2022-11-08 19:21:11 -0300

    deploy contract from another contract

---
+++ StorageFactory.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+//inheritance
+contract StorageFactory is SimpleStorage {
+    
+    SimpleStorage[] public SSArray;
+
+    function createSSC() public{
+        SimpleStorage SS = new SimpleStorage();
+        SSArray.push(SS);
+    }
+
+    function sfStore(uint256 ssIndex, uint256 ssNumber) public {
+        //address, ABI
+        SimpleStorage(address(SSArray[ssIndex])).store(ssNumber);
+    }
+
+    function sfGet(uint256 ssIndex) public view returns (uint256){
+        return SimpleStorage(address(SSArray[ssIndex])).retrieve();
+    }
+}
